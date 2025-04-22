7e7c69309a77480c33a0173fab28da80479838e6
RAJESH KUMAR <68324051+iiitchamp@users.noreply.github.com>
2025-04-22 23:49:34 +0530

    Update exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,1 +1,19 @@
+class TransformationError(Exception):
+    """Base class for transformation errors."""
+    pass
 
+class InvalidConfigurationError(TransformationError):
+    """Raised when configuration is invalid."""
+    pass
+
+class UnsupportedTransformationError(TransformationError):
+    """Raised when an unsupported transformation is requested."""
+    pass
+
+class MissingParameterError(TransformationError):
+    """Raised when required parameters are missing."""
+    pass
+
+class ActionExecutionError(TransformationError):
+    """Raised when an action fails to execute."""
+    pass
